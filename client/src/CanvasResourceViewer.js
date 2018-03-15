3d205711ed0f30decb893bbfcc81bc2711f1da3a
Andy Stuhl <andykstuhl@gmail.com>
2018-03-15 00:22:10 -0500

    use customized react-scripts with openseadragon shim; render dot with paper js in overlay

--- client/src/CanvasResourceViewer.js
+++ client/src/CanvasResourceViewer.js
@@ -1,21 +1,24 @@
 import React, {Component} from 'react';
 import OpenSeadragon from 'openseadragon';
-// import Annotations from 'openseadragon-annotations';
+import 'openseadragon-paperjs-overlay';
 import paper from 'paper';
-import paperjsOverlay from 'openseadragon-paperjs-overlay';
 
 export default class CanvasResourceViewer extends Component {
   componentDidMount() {
     const viewer = OpenSeadragon({
       id: 'openseadragon1',
-      prefixUrl: 'img/openseadragon',
+      prefixUrl: 'openseadragon/build/openseadragon/images',
       tileSources: {
         type: 'image',
         url: 'https://upload.wikimedia.org/wikipedia/commons/9/94/USS_Lexington_%28CV-2%29_leaving_San_Diego_on_14_October_1941_%2880-G-416362%29.jpg'
       },
-      debugMode: true
+      debugMode: false
     });
-    // new OpenSeadragon.Annotations({ viewer });
+      const overlay = viewer.paperjsOverlay();
+      let circle = new paper.Path.Circle(new paper.Point(1000, 1000), 200);
+      circle.fillColor = 'red';
+      overlay.resize();
+      overlay.resizecanvas();
   }
 
   render() {
