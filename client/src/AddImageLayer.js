ed72c97781d6d914b5564a4c9ae5d6af02073770
Nick Laiacona <nick@performantsoftware.com>
2019-01-04 11:16:09 -0500

    improved image upload ui

--- client/src/AddImageLayer.js
+++ client/src/AddImageLayer.js
@@ -7,15 +7,17 @@ import MenuItem from 'material-ui/MenuItem';
 import TextField from 'material-ui/TextField';
 import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
+import { Snackbar } from 'material-ui';
 
 import CloudUpload from 'material-ui/svg-icons/file/cloud-upload';
+import InsertLink from 'material-ui/svg-icons/editor/insert-link';
 
 import { setAddTileSourceMode, IIIF_TILE_SOURCE_TYPE, IMAGE_URL_SOURCE_TYPE, UPLOAD_SOURCE_TYPE } from './modules/canvasEditor';
 import { replaceDocument, updateDocument, setDocumentThumbnail } from './modules/documentGrid';
 
 const tileSourceTypeLabels = {};
-tileSourceTypeLabels[IIIF_TILE_SOURCE_TYPE] = {select: 'IIIF', textField: 'IIIF info.json URL'};
-tileSourceTypeLabels[IMAGE_URL_SOURCE_TYPE] = {select: 'Image URL', textField: 'External static image URL'};
+tileSourceTypeLabels[IIIF_TILE_SOURCE_TYPE] = {select: 'IIIF', textField: 'Link to IIIF Image'};
+tileSourceTypeLabels[IMAGE_URL_SOURCE_TYPE] = {select: 'Image URL', textField: 'Link to Web Image'};
 tileSourceTypeLabels[UPLOAD_SOURCE_TYPE] = {select: 'Upload image', textField: 'Choose files'};
 
 class AddImageLayer extends Component {
@@ -87,9 +89,8 @@ class AddImageLayer extends Component {
       this.props.setDocumentThumbnail(this.props.document_id, this.imageUrlForThumbnail);
   }
 
-  renderUploadButton(buttonStyle) {
+  renderUploadButton(buttonStyle,iconStyle) {
     const { document_id, replaceDocument } = this.props;
-
     return (
         <ActiveStorageProvider
             endpoint={{
@@ -100,16 +101,18 @@ class AddImageLayer extends Component {
             }}
             multiple={true}
             onSubmit={document => {
+                this.props.setAddTileSourceMode(this.props.document_id, UPLOAD_SOURCE_TYPE);
                 replaceDocument(document);
                 this.addTileSource(UPLOAD_SOURCE_TYPE);
             }}
             render={({ handleUpload, uploads, ready}) => (
             <RaisedButton
                 containerElement='label'
-                label='Upload Files'
-                icon={<CloudUpload/>}
                 style={buttonStyle}
-            >
+                icon={<CloudUpload style={iconStyle}/>}
+                label='Upload from Computer'
+            >                
+                { this.renderUploadMessage(uploads) }
                 <input
                 key='upload-form'
                 type='file'
@@ -124,32 +127,67 @@ class AddImageLayer extends Component {
     );
   }
 
+  onIIIFLink = () => {
+    this.props.setAddTileSourceMode(this.props.document_id, IIIF_TILE_SOURCE_TYPE);
+  }
+
+  onWebLink = () => {
+    this.props.setAddTileSourceMode(this.props.document_id, IMAGE_URL_SOURCE_TYPE);
+  }
+
+  onLinkSubmit = () => {
+    const { document_id, addTileSourceMode } = this.props;
+    const tileSourceMode = addTileSourceMode[document_id];
+    this.addTileSource(tileSourceMode);
+  }
+
   onCancel = () => {
     this.props.setAddTileSourceMode(this.props.document_id, null);
+  }
+
+  renderUploadMessage(uploads) {
+    const message = uploads.map(
+        upload =>
+        upload.state === 'waiting' || 'uploading' ? (
+            `Uploading...`
+        ) : upload.state === 'error' ? (      
+            `Error uploading ${upload.file.name}: ${upload.error}`
+        ) : (
+            `Finished uploading ${upload.file.name}`
+        )
+    );
+
+    return (
+        <Snackbar
+            message={message}        
+            autoHideDuration={4000}
+        />
+    )
   }
 
   render() {
     const { document_id, addTileSourceMode } = this.props;
     const tileSourceMode = addTileSourceMode[document_id];
     const textStyle = { color: 'white' };
-    const buttonStyle = { margin: 12, display: 'inline' };
-    
+    const buttonStyle = { margin: 12, height: 60 };
+    const iconStyle = { width: 50, height: 50}
+
     return (
         <div style={{ display: tileSourceMode && this.props.writeEnabled ? 'initial' : 'none' }} >
             <h2 style={textStyle}>Add an Image</h2>
             <p style={textStyle}>Choose an image source type.</p>
             <div>
-                { this.renderUploadButton(buttonStyle) }
+                { this.renderUploadButton(buttonStyle,iconStyle) }
                 <RaisedButton
-                        label='IIIF URL'
-                        icon={<CloudUpload/>}
-                        onClick={this.onCancel}
+                        label='Link to IIIF Image'
+                        icon={<InsertLink style={iconStyle}/>}
+                        onClick={this.onIIIFLink}
                         style={buttonStyle}
                 />
                 <RaisedButton
-                        label='WWW URL'
-                        icon={<CloudUpload/>}
-                        onClick={this.onCancel}
+                        label='Link to Web Image'
+                        icon={<InsertLink style={iconStyle}/>}
+                        onClick={this.onWebLink}
                         style={buttonStyle}
                 />
             </div>
@@ -163,6 +201,11 @@ class AddImageLayer extends Component {
                         floatingLabelText={tileSourceMode ? tileSourceTypeLabels[tileSourceMode].textField : ''}
                         onChange={(event, newValue) => {this.newTileSourceValue = newValue;}}
                     />
+                    <RaisedButton
+                        label='Add Image'
+                        style={ {marginLeft: 12} }
+                        onClick={this.onLinkSubmit}
+                    />
                 </div>
             }
 
@@ -171,92 +214,6 @@ class AddImageLayer extends Component {
     );
   }
 
-
-  renderold() {
-    const { document_id, image_thumbnail_urls, addTileSourceMode, setAddTileSourceMode, replaceDocument } = this.props;
-    const mode = addTileSourceMode[document_id];
-    
-    return (
-        <div style={{ display: mode && this.props.writeEnabled ? 'initial' : 'none' }} >
-            <SelectField
-                style={{ color: 'white' }}
-                labelStyle={{ color: 'white' }}
-                floatingLabelStyle={{ color: 'white' }}
-                floatingLabelText='Image source type'
-                value={addTileSourceMode[document_id]}
-                onChange={(event, index, newValue) => {setAddTileSourceMode(document_id, newValue);}}
-            >
-                <MenuItem value={UPLOAD_SOURCE_TYPE} primaryText={tileSourceTypeLabels[UPLOAD_SOURCE_TYPE].select} />
-                <MenuItem value={IIIF_TILE_SOURCE_TYPE} primaryText={tileSourceTypeLabels[IIIF_TILE_SOURCE_TYPE].select} />
-                <MenuItem value={IMAGE_URL_SOURCE_TYPE} primaryText={tileSourceTypeLabels[IMAGE_URL_SOURCE_TYPE].select} />
-            </SelectField>
-            {addTileSourceMode[document_id] !== UPLOAD_SOURCE_TYPE &&
-                <div>
-                <TextField
-                    id={this.osdId + '-addtilesource'}
-                    inputStyle={{ color: 'white' }}
-                    floatingLabelStyle={{ color: 'white' }}
-                    floatingLabelText={mode ? tileSourceTypeLabels[mode].textField : ''}
-                    onChange={(event, newValue) => {this.newTileSourceValue = newValue;}}
-                />
-                <br /><br />
-                </div>
-            }
-            {addTileSourceMode[document_id] === UPLOAD_SOURCE_TYPE &&
-                <ActiveStorageProvider
-                endpoint={{
-                    path: `/documents/${document_id}/add_images`,
-                    model: 'Document',
-                    attribute: 'images',
-                    method: 'PUT'
-                }}
-                multiple={true}
-                onSubmit={document => {
-                    replaceDocument(document);
-                }}
-                render={({ handleUpload, uploads, ready}) => (
-                    <div>
-                    <RaisedButton
-                        containerElement='label'
-                        label={mode ? tileSourceTypeLabels[mode].textField : 'Choose files'}
-                    >
-                        <input
-                        type='file'
-                        multiple={true}
-                        disabled={!ready}
-                        onChange={e => handleUpload(e.currentTarget.files)}
-                        style={{ display: 'none' }}
-                        />
-                    </RaisedButton>
-                    {uploads.map(
-                        upload =>
-                        upload.state === 'waiting' ? (
-                            <p style={{ color: 'white' }} key={upload.id}>Waiting to upload {upload.file.name}</p>
-                        ) : upload.state === 'uploading' ? (
-                            <p style={{ color: 'white' }}  key={upload.id}>
-                            Uploading {upload.file.name}: {upload.progress}%
-                            </p>
-                        ) : upload.state === 'error' ? (
-                            <p style={{ color: 'white' }}  key={upload.id}>
-                            Error uploading {upload.file.name}: {upload.error}
-                            </p>
-                        ) : (
-                            <p style={{ color: 'white' }}  key={upload.id}>Finished uploading {upload.file.name}</p>
-                        )
-                    )}
-                    <br /><br />
-                    {image_thumbnail_urls.map((thumbnailUrl, index) =>
-                        <img alt='' key={`thumbnail-${document_id}-${index}`} src={thumbnailUrl} style={{ maxWidth: '40px', maxHeight: '40px' }} />
-                    )}
-                    </div>
-                )}
-                />
-            }
-            <FlatButton label='Cancel' style={{ color: 'white' }} onClick={() => {setAddTileSourceMode(document_id, null);}} />
-            <FlatButton label='Add image source' style={{ color: 'white' }} onClick={this.addTileSource.bind(this)} />
-        </div>
-    );
-  }
 }
 
 const mapStateToProps = state => ({
