d79f6a2e0f83c5ab62ae3f47e07a1bea5d1b9960
Nick Laiacona <nick@performantsoftware.com>
2019-01-11 08:47:11 -0500

    refactoring

--- client/src/ProjectSettingsDialog.js
+++ client/src/ProjectSettingsDialog.js
@@ -23,10 +23,13 @@ import { red100, red200, red400, red600, lightBlue100, lightBlue200, grey200, gr
 import { hideSettings, updateProject, setNewPermissionUser, setNewPermissionLevel, createNewPermission, deletePermission, updatePermission, toggleDeleteConfirmation, deleteProject, READ_PERMISSION, WRITE_PERMISSION, ADMIN_PERMISSION } from './modules/project';
 
 class ProjectSettingsDialog extends Component {
-  render() {
+
+  renderAddCollabRow() {
+    const { currentUser, allUsers, newPermissionLevel, userProjectPermissions, newPermissionUser } = this.props; 
+
     let usersDataSource = [];
-    const permissionIds = this.props.userProjectPermissions.map(userProjectPermission => userProjectPermission.user.id);
-    this.props.allUsers.forEach(user => {
+    const permissionIds = userProjectPermissions.map(userProjectPermission => userProjectPermission.user.id);
+    allUsers.forEach(user => {
       if (!permissionIds.includes(user.id)) {
         usersDataSource.push({
           textKey: user.name,
@@ -40,130 +43,170 @@ class ProjectSettingsDialog extends Component {
     };
 
     return (
+      <TableRow>
+        <TableRowColumn>
+          <AutoComplete
+            hintText='Add a collaborator'
+            filter={AutoComplete.noFilter}
+            openOnFocus={true}
+            dataSource={usersDataSource}
+            dataSourceConfig={usersDataSourceConfig}
+            onNewRequest={(chosenRequest, index) => {
+              if (index >= 0) {
+                this.props.setNewPermissionUser(usersDataSource[index].valueKey);
+              }
+            }}
+          />
+        </TableRowColumn>
+        {currentUser.attributes.admin &&
+          <TableRowColumn></TableRowColumn>
+        }
+        <TableRowColumn>
+          <SelectField
+            value={newPermissionLevel}
+            onChange={(event, index, newValue) => {this.props.setNewPermissionLevel(newValue);}}
+          >
+            <MenuItem value={READ_PERMISSION} primaryText='Read' />
+            <MenuItem value={WRITE_PERMISSION} primaryText='Write' />
+            <MenuItem value={ADMIN_PERMISSION} primaryText='Admin' />
+          </SelectField>
+        </TableRowColumn>
+        <TableRowColumn>
+          <FlatButton label='Add' disabled={newPermissionUser === null} onClick={this.props.createNewPermission} backgroundColor={lightBlue100} hoverColor={lightBlue200} />
+        </TableRowColumn>
+      </TableRow>
+    )
+  }
+
+  renderCollabTableRow(userProjectPermission) {
+    const { currentUser } = this.props;
+
+    return (
+      <TableRow key={userProjectPermission.id}>
+        <TableRowColumn>{userProjectPermission.user.name}</TableRowColumn>
+        {currentUser.attributes.admin &&
+          <TableRowColumn>{userProjectPermission.user.user_email}</TableRowColumn>
+        }
+        <TableRowColumn>
+          <SelectField
+            value={userProjectPermission.permission}
+            onChange={(event, index, newValue) => {this.props.updatePermission(userProjectPermission.id, newValue);}}
+            disabled={currentUser.attributes.id === userProjectPermission.user.id}
+          >
+            <MenuItem value={READ_PERMISSION} primaryText='Read' />
+            <MenuItem value={WRITE_PERMISSION} primaryText='Write' />
+            <MenuItem value={ADMIN_PERMISSION} primaryText='Admin' />
+          </SelectField>
+        </TableRowColumn>
+        <TableRowColumn>
+          <FlatButton label='Revoke' onClick={() => {this.props.deletePermission(userProjectPermission.id);}} backgroundColor={red100} hoverColor={red200} disabled={currentUser.attributes.id === userProjectPermission.user.id} />
+        </TableRowColumn>
+      </TableRow>
+    )
+  }
+
+  renderCollaboratorsTab() {
+    const { userProjectPermissions, currentUser } = this.props;
+
+    return (
+      <Tab label='Collaborators'>
+        <Table selectable={false}>
+          <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+            <TableRow>
+              <TableHeaderColumn>Name</TableHeaderColumn>
+              { currentUser.attributes.admin &&
+                <TableHeaderColumn>Email</TableHeaderColumn>
+              }
+              <TableHeaderColumn>Permission level</TableHeaderColumn>
+              <TableHeaderColumn></TableHeaderColumn>
+            </TableRow>
+          </TableHeader>
+          <TableBody displayRowCheckbox={false}>
+            { userProjectPermissions.map(userProjectPermission => {
+              return this.renderCollabTableRow(userProjectPermission)
+            })}
+            { this.renderAddCollabRow() }
+          </TableBody>
+        </Table>
+      </Tab>
+    );
+  }
+
+  renderProjectTab() {
+    const { title, description } = this.props;
+
+    return (
+      <Tab label='Project'>
+        <TextField
+          defaultValue={title}
+          floatingLabelText='Title'
+          onChange={(event, newValue) => {this.scheduleProjectTitleUpdate(newValue);}}
+        /><br />
+        <TextField
+          defaultValue={description}
+          floatingLabelText='Description'
+          onChange={(event, newValue) => {this.scheduleProjectDescriptionUpdate(newValue);}}
+          multiLine={true}
+          rows={4}
+          rowsMax={2}
+        />
+      </Tab>
+    )
+  }
+
+  renderDeleteTab() {
+    const { id, deleteConfirmed, toggleDeleteConfirmation } = this.props;
+
+    return (
+      <Tab label='Publishing & Deletion'>
+        <Toggle
+          label='Make project publicly viewable'
+          toggled={this.props.public}
+          onToggle={(event, isInputChecked) => {this.props.updateProject(id, {public: isInputChecked});}}
+          style={{ maxWidth: '300px', margin: '18px 0' }}
+        />
+        <Divider />
+        <Checkbox
+          label='I understand that by destroying this project, I will permanently destroy all documents, folders, and highlights associated with it.'
+          checked={deleteConfirmed}
+          onCheck={toggleDeleteConfirmation}
+          style={{ maxWidth: '500px', margin: '18px 0' }}
+        />
+        <FlatButton
+          label='Destroy project'
+          onClick={() => {deleteProject(id);}}
+          backgroundColor={red400}
+          hoverColor={red600}
+          style={{ color: deleteConfirmed ? 'white' : grey200 }}
+          disabled={!deleteConfirmed}
+        />
+      </Tab>
+    )
+  }
+
+  render() {
+    const { settingsShown, hideSettings } = this.props;
+
+    return (
       <Dialog
         title='Project settings'
         modal={false}
-        open={this.props.settingsShown}
-        onRequestClose={this.props.hideSettings}
+        open={settingsShown}
+        onRequestClose={hideSettings}
         autoScrollBodyContent={true}
         actions={[
           <FlatButton
             label='Close'
             primary={true}
-            onClick={this.props.hideSettings}
+            onClick={hideSettings}
           />
         ]}
         contentStyle={{ width: '90%', maxWidth: '1000px' }}
       >
         <Tabs tabItemContainerStyle={{ background: grey600 }}>
-          <Tab label='Project'>
-            <TextField
-              defaultValue={this.props.title}
-              floatingLabelText='Title'
-              onChange={(event, newValue) => {this.scheduleProjectTitleUpdate(newValue);}}
-            /><br />
-            <TextField
-              defaultValue={this.props.description}
-              floatingLabelText='Description'
-              onChange={(event, newValue) => {this.scheduleProjectDescriptionUpdate(newValue);}}
-              multiLine={true}
-              rows={4}
-              rowsMax={2}
-            />
-          </Tab>
-          <Tab label='Collaborators'>
-            <Table selectable={false}>
-              <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
-                <TableRow>
-                  <TableHeaderColumn>Name</TableHeaderColumn>
-                  {this.props.currentUser.attributes.admin &&
-                    <TableHeaderColumn>Email</TableHeaderColumn>
-                  }
-                  <TableHeaderColumn>Permission level</TableHeaderColumn>
-                  <TableHeaderColumn></TableHeaderColumn>
-                </TableRow>
-              </TableHeader>
-              <TableBody displayRowCheckbox={false}>
-                {this.props.userProjectPermissions.map(userProjectPermission => (
-                  <TableRow key={userProjectPermission.id}>
-                    <TableRowColumn>{userProjectPermission.user.name}</TableRowColumn>
-                    {this.props.currentUser.attributes.admin &&
-                      <TableRowColumn>{userProjectPermission.user.user_email}</TableRowColumn>
-                    }
-                    <TableRowColumn>
-                      <SelectField
-                        value={userProjectPermission.permission}
-                        onChange={(event, index, newValue) => {this.props.updatePermission(userProjectPermission.id, newValue);}}
-                        disabled={this.props.currentUser.attributes.id === userProjectPermission.user.id}
-                      >
-                        <MenuItem value={READ_PERMISSION} primaryText='Read' />
-                        <MenuItem value={WRITE_PERMISSION} primaryText='Write' />
-                        <MenuItem value={ADMIN_PERMISSION} primaryText='Admin' />
-                      </SelectField>
-                    </TableRowColumn>
-                    <TableRowColumn>
-                      <FlatButton label='Revoke' onClick={() => {this.props.deletePermission(userProjectPermission.id);}} backgroundColor={red100} hoverColor={red200} disabled={this.props.currentUser.attributes.id === userProjectPermission.user.id} />
-                    </TableRowColumn>
-                  </TableRow>
-                ))}
-                <TableRow>
-                  <TableRowColumn>
-                    <AutoComplete
-                      hintText='Add a collaborator'
-                      filter={AutoComplete.noFilter}
-                      openOnFocus={true}
-                      dataSource={usersDataSource}
-                      dataSourceConfig={usersDataSourceConfig}
-                      onNewRequest={(chosenRequest, index) => {
-                        if (index >= 0) {
-                          this.props.setNewPermissionUser(usersDataSource[index].valueKey);
-                        }
-                      }}
-                    />
-                  </TableRowColumn>
-                  {this.props.currentUser.attributes.admin &&
-                    <TableRowColumn></TableRowColumn>
-                  }
-                  <TableRowColumn>
-                    <SelectField
-                      value={this.props.newPermissionLevel}
-                      onChange={(event, index, newValue) => {this.props.setNewPermissionLevel(newValue);}}
-                    >
-                      <MenuItem value={READ_PERMISSION} primaryText='Read' />
-                      <MenuItem value={WRITE_PERMISSION} primaryText='Write' />
-                      <MenuItem value={ADMIN_PERMISSION} primaryText='Admin' />
-                    </SelectField>
-                  </TableRowColumn>
-                  <TableRowColumn>
-                    <FlatButton label='Add' disabled={this.props.newPermissionUser === null} onClick={this.props.createNewPermission} backgroundColor={lightBlue100} hoverColor={lightBlue200} />
-                  </TableRowColumn>
-                </TableRow>
-              </TableBody>
-            </Table>
-          </Tab>
-          <Tab label='Publishing & Deletion'>
-            <Toggle
-              label='Make project publicly viewable'
-              toggled={this.props.public}
-              onToggle={(event, isInputChecked) => {this.props.updateProject(this.props.id, {public: isInputChecked});}}
-              style={{ maxWidth: '300px', margin: '18px 0' }}
-            />
-            <Divider />
-            <Checkbox
-              label='I understand that by destroying this project, I will permanently destroy all documents, folders, and highlights associated with it.'
-              checked={this.props.deleteConfirmed}
-              onCheck={this.props.toggleDeleteConfirmation}
-              style={{ maxWidth: '500px', margin: '18px 0' }}
-            />
-            <FlatButton
-              label='Destroy project'
-              onClick={() => {this.props.deleteProject(this.props.id);}}
-              backgroundColor={red400}
-              hoverColor={red600}
-              style={{ color: this.props.deleteConfirmed ? 'white' : grey200 }}
-              disabled={!this.props.deleteConfirmed}
-            />
-          </Tab>
+          { this.renderProjectTab() }
+          { this.renderCollaboratorsTab() }
+          { this.renderDeleteTab() }
         </Tabs>
       </Dialog>
     );
