d316ba05fab9d9c972df0426d08fd0195e4f1bbf
Nick Laiacona <nick@performantsoftware.com>
2019-01-23 09:34:37 -0500

    adjust layout of toolbar

--- client/src/AddDocumentButton.js
+++ client/src/AddDocumentButton.js
@@ -12,8 +12,8 @@ export default class AddDocumentButton extends Component {
       <div>
         <FlatButton
           label={this.props.label}
+          style={{marginLeft: 10}}
           icon={<NoteAdd />}
-          style={{margin: 'auto'}}
           onClick={this.props.openDocumentPopover}
           id={buttonId}
         />
