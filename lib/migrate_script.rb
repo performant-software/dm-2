2fab50c5668cb3e29565210ef36ef885660a2f9d
Nick Laiacona <nick@performantsoftware.com>
2019-03-04 10:45:16 -0500

    add ability to globally remap image urls

--- lib/migrate_script.rb
+++ lib/migrate_script.rb
@@ -16,3 +16,21 @@
       end
     }
   end
+
+  def modify_image_urls( match_str, replace_str )
+    documents = Document.where(document_kind: 'canvas')
+    documents.each { |document|
+      content = document.content
+      if content["tileSources"] 
+        changed = false
+        content["tileSources"].each { |tileSource|
+          url = tileSource["url"]
+          if url && url.match?(match_str)
+            tileSource["url"] = url.sub(match_str,replace_str)
+            changed = true
+          end
+        }
+        document.save! if changed
+      end
+    }
+  end
