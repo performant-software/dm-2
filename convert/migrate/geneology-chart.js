2956afee50dda4389f1d86eed38857ff937b0554
Nick Laiacona <nick@performantsoftware.com>
2019-11-17 07:54:06 -0500

    improved math

--- convert/migrate/geneology-chart.js
+++ convert/migrate/geneology-chart.js
@@ -4,6 +4,7 @@ const Sequelize = require('sequelize');
 // https://docs.google.com/spreadsheets/d/1499c-8yeIft-qq9Qya4q-Alj_XxJg37GE91uUmG429Y/edit?usp=sharing
 const imageScaleFactor = 2.065
 const imageOffset = 5
+const imageToSVGScaleFactor = 0.5693
 
 function createHightlightsTable(sequelize) {
     class Highlights extends Sequelize.Model {}
@@ -12,6 +13,9 @@ function createHightlightsTable(sequelize) {
       id: {
           type: Sequelize.INTEGER, 
           primaryKey: true
+      },
+      document_id: {
+        type: Sequelize.INTEGER
       },
       target: {
         type: Sequelize.STRING,
@@ -26,6 +30,35 @@ function createHightlightsTable(sequelize) {
   return Highlights
 }
 
+// transform the highlights for document 731
+async function transformAllHighlights(sequelize) {
+    const hightlightsTable = createHightlightsTable(sequelize)
+    const highlights = await hightlightsTable.findAll( { where: { document_id: 731 } })
+    for( const highlight of highlights ) {
+      const shape = JSON.parse(highlight.target)
+      shape.top = ((shape.top * imageScaleFactor ) + imageOffset) * imageToSVGScaleFactor
+      await hightlightsTable.update( { target: JSON.stringify(shape) }, {
+        where: {
+            id: highlight.id
+        }
+      })
+    }
+}
+
+// for debugging transform math
+async function testTransform(sequelize) {
+  const highlightID = 13789 
+  const hightlightsTable = createHightlightsTable(sequelize)
+  let testHighlight = await hightlightsTable.findByPk(highlightID)
+  const shape = JSON.parse(testHighlight.target)
+  // shape.top = ((shape.top * imageScaleFactor ) + imageOffset) * imageToSVGScaleFactor
+  shape.top = 5260.81
+  await hightlightsTable.update( { target: JSON.stringify(shape) }, {
+    where: {
+        id: highlightID
+    }
+  })
+}
 
 async function main() {
 
@@ -36,21 +69,11 @@ async function main() {
           timestamps: false
         }
     })
-
-    // TODO grab all of the highlights for document 731
-
-    const hightlightsTable = createHightlightsTable(sequelize)
-    let testHighlight = await hightlightsTable.findByPk(11672)
-    const shape = JSON.parse(testHighlight.target)
-    shape.top = (shape.top * imageScaleFactor ) + imageOffset
-    // shape.top = 283.625
-    shape.top = 255.294
     
-    await hightlightsTable.update( { target: JSON.stringify(shape) }, {
-        where: {
-            id: 11672
-        }
-    })
+    // await testTransform(sequelize)
+    await transformAllHighlights(sequelize)
+    
+    await sequelize.close()
 }
 
 // RUN
