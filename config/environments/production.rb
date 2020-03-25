b9ab84d88c1326f28d4ca03152e7de2486a42d43
Derek Leadbetter <dleadbetter55@gmail.com>
2020-03-25 11:19:56 -0400

    DM2 #254 - Changing default protocol to 'http' instead of 'https' (like the force_ssl property, this may need to change in the future)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,4 +85,4 @@ Rails.application.configure do
 end
 
 Rails.application.routes.default_url_options[:host] = ENV['HOSTNAME']
-Rails.application.routes.default_url_options[:protocol] = 'https'
+Rails.application.routes.default_url_options[:protocol] = 'http'
