cd57bb53d86621b6c3ef8c2a8d328be9fb406a0c
Ben Silverman <ben@performantsoftware.com>
2021-07-13 14:21:24 -0400

    fix: Don't show popover if highlights are hidden

--- client/src/Project.js
+++ client/src/Project.js
@@ -38,12 +38,15 @@ class Project extends Component {
   }
 
   selectTextHighlight(document_id, highlight_id) {
-    if (this.props.highlightSelectModes[document_id]) {
-      this.props.selectHighlight(document_id, highlight_id);
-    }
-    // if the clicked highlight is currently selected, don't proceed with the normal popover behavior to facilitate editing the highlighted text
-    else if (this.props.selectedHighlights[document_id] !== highlight_id) {
-      this.setFocusHighlight(document_id, highlight_id);
+    // Don't show highlight's popover if doc's highlights are hidden
+    if (!this.props.highlightsHidden[document_id]) {
+      if (this.props.highlightSelectModes[document_id]) {
+        this.props.selectHighlight(document_id, highlight_id);
+      }
+      // if the clicked highlight is currently selected, don't proceed with the normal popover behavior to facilitate editing the highlighted text
+      else if (this.props.selectedHighlights[document_id] !== highlight_id) {
+        this.setFocusHighlight(document_id, highlight_id);
+      }
     }
   }
 
