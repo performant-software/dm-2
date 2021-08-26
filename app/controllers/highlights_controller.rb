b971799769f0a781b9f305e47a3f4067e60120c9
Ben Silverman <ben@performantsoftware.com>
2021-08-26 10:19:18 -0400

    fix!: Add title field to highlights
    BREAKING CHANGE: Requires a database migration

--- app/controllers/highlights_controller.rb
+++ app/controllers/highlights_controller.rb
@@ -114,6 +114,6 @@ class HighlightsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def highlight_params
-      params.require(:highlight).permit(:uid, :target, :document_id, :excerpt, :color)
+      params.require(:highlight).permit(:uid, :target, :document_id, :excerpt, :color, :title)
     end
 end
