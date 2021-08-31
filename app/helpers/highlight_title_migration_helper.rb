61f4b7a970c6c97808616a4ef74e934144a4eed4
Ben Silverman <ben@performantsoftware.com>
2021-08-31 12:05:39 -0400

    fix: Fix highlight helper to use batch find_each

--- app/helpers/highlight_title_migration_helper.rb
+++ app/helpers/highlight_title_migration_helper.rb
@@ -2,9 +2,9 @@ module HighlightTitleMigrationHelper
   # One time migration function for 20210826133446_add_title_to_highlights.rb
   # Optional - only run if existing excerpts should be converted to titles
   def self.convert_excerpts_to_titles!
-    Highlight.all.each { |hl|
+    Highlight.find_each do |hl|
       excerpt = hl[:excerpt]
       hl.update(:title => excerpt)
-    }
+    end
   end
 end
