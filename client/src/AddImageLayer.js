1e8a8be6084f1630fbfa9aa92e5fe7331158eaa4
Ben Silverman <ben@performantsoftware.com>
2021-08-31 10:18:26 -0400

    fix: Fix multiple layer add after multi upload

--- client/src/AddImageLayer.js
+++ client/src/AddImageLayer.js
@@ -49,12 +49,21 @@ class AddImageLayer extends Component {
   }
 
   componentDidUpdate(prevProps) {
+    if (this.props.addTileSourceMode[this.props.document_id] !== prevProps.addTileSourceMode[this.props.document_id]) {
+      console.log('tile source mode changed');
+      console.log(this.props.addTileSourceMode[this.props.document_id]);
+    }
     if (this.props.image_urls.length > prevProps.image_urls.length) {
-      this.props.image_urls.forEach(url =>{
-        if (!prevProps.image_urls.includes(url)) {
-          this.setState((prevState) => ({ ...prevState, newImageUrls: prevState.newImageUrls.concat([url]) }));
-        }
-      })
+      console.log('image urls changed');
+      if (this.props.addTileSourceMode[this.props.document_id] === UPLOAD_SOURCE_TYPE) {
+        this.props.image_urls.forEach(url =>{
+          if (!prevProps.image_urls.includes(url)) {
+            this.setState((prevState) => ({ ...prevState, newImageUrls: prevState.newImageUrls.concat([url]) }));
+          }
+        })
+      } else {
+        this.setState({ newImageUrls: [] });
+      }
     }
     if (prevProps.content && this.props.content 
         && this.props.content.tileSources && this.props.content.tileSources[0]
