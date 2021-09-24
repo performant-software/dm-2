f022f13eaeee581e7ec5d4f9838673a94ee15743
Ben Silverman <ben@performantsoftware.com>
2021-09-24 14:30:31 -0400

    fix: Sidekiq fixes for heroku

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,18 @@
 require 'sidekiq'
 require 'sidekiq-status'
 
+SIDEKIQ_REDIS_CONFIGURATION = {
+  url: ENV[ENV["REDIS_PROVIDER"]],
+  ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }, # we must trust Heroku and AWS here
+}
+
 Sidekiq.configure_client do |config|
+  config.redis = SIDEKIQ_REDIS_CONFIGURATION
   Sidekiq::Status.configure_client_middleware config, expiration: 30.minutes
 end
 
 Sidekiq.configure_server do |config|
+  config.redis = SIDEKIQ_REDIS_CONFIGURATION
   Sidekiq::Status.configure_server_middleware config, expiration: 30.minutes
   Sidekiq::Status.configure_client_middleware config, expiration: 30.minutes
 end
