b3f527f228072055876ad495667ff3f1a8803653
Ben Silverman <ben@performantsoftware.com>
2021-09-30 10:17:21 -0400

    fix: Reduce batch size for migration functions

--- app/helpers/search_text_helper.rb
+++ app/helpers/search_text_helper.rb
@@ -2,7 +2,7 @@ module SearchTextHelper
   def self.patch_search_text!
     require 'storyblok/richtext'
     renderer = Storyblok::Richtext::HtmlRenderer.new
-    Document.where(:document_kind => 'text').find_each do |text_doc|
+    Document.where(:document_kind => 'text').find_each(batch_size: 200) do |text_doc|
       html = renderer.render(text_doc[:content])
       searchable = render_to_ascii(Nokogiri::HTML(html))
       text_doc.update!(:search_text => searchable)
