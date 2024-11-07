b3ced320586f0cdcedbf5dba0e82d557dc1d97f4
Ben Silverman <ben@performantsoftware.com>
2024-11-07 13:57:55 -0500

    Allow document permalink feature in iframe embed instances

---
+++ public/embed.js
@@ -0,0 +1,9 @@
+// include the following code in <script> tags on the same page as an embedded instance
+const frame = document.querySelector("iframe");
+// allow writing to the clipboard from another domain
+frame.setAttribute("allow", "clipboard-write *");
+const queryParams = window.location.search;
+if (queryParams !== "") {
+  // pass query params to iframe embed's src attribute (app url)
+  frame.src = frame.src + queryParams;
+}
