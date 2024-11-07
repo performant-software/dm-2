9b9620cda68fb37f4d3122120382484a715cc602
Ben Silverman <ben@performantsoftware.com>
2024-11-07 14:32:42 -0500

    Simplify embed code for iframes

--- public/embed.js
+++ public/embed.js
@@ -3,10 +3,6 @@ const frame = document.querySelector("iframe");
 // allow writing to the clipboard from another domain
 frame.setAttribute("allow", "clipboard-write *");
 const queryParams = window.location.search;
-if (queryParams !== "") {
-  // pass query params to iframe embed's src attribute (app url)
-  frame.src = frame.src + queryParams;
-} else {
-  // refresh iframe content to apply clipboard-write permission
-  frame.src = frame.src;
-}
+// pass query params to iframe embed's src attribute (app url)
+frame.src = frame.src + queryParams;
+// (if they are not present, will refresh frame src and apply clipboard-write perms)
