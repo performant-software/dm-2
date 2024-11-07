a45e2050d4e0a5f8103d584f4c5bb02ea84088b5
Ben Silverman <ben@performantsoftware.com>
2024-11-07 14:23:45 -0500

    Ensure clipboard-write permission is set properly on embed

--- public/embed.js
+++ public/embed.js
@@ -6,4 +6,7 @@ const queryParams = window.location.search;
 if (queryParams !== "") {
   // pass query params to iframe embed's src attribute (app url)
   frame.src = frame.src + queryParams;
+} else {
+  // refresh iframe content to apply clipboard-write permission
+  frame.src = frame.src;
 }
